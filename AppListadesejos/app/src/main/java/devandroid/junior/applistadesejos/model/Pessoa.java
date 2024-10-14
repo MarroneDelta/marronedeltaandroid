ebe2902c75a68317bf92601f81c55535ac047012
MarroneDelta <marronedelta777@gmail.com>
2024-10-13 21:40:34 -0300

    Criacao de classe Pessoa..

--- AppListadesejos/app/src/main/java/devandroid/junior/applistadesejos/model/Pessoa.java
+++ AppListadesejos/app/src/main/java/devandroid/junior/applistadesejos/model/Pessoa.java
@@ -4,6 +4,52 @@ public class Pessoa {
 
     // Apos cirar a Classe precisamos de um Construtor;
     // Criar construtor;
+    //Atributos estao relacionados a Objetos da Classe Pessoa; como se fosse um template
+
+
+    private String primeiroNome;
+    private String sobreNome;
+    private String nomedoCurso;
+    private String telefonedeContato;
+
+    // Criaremos os metodos de acesso aos atributos criados:
+    // Metodos de acesso: Get(permite pegar o que esta salvo naquele atributo)
+    // Set(permite atribuir algum valor para aquele atributo)
+
+
+
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getNomedoCurso() {
+        return nomedoCurso;
+    }
+
+    public void setNomedoCurso(String nomedoCurso) {
+        this.nomedoCurso = nomedoCurso;
+    }
+
+    public String getTelefonedeContato() {
+        return telefonedeContato;
+    }
+
+    public void setTelefonedeContato(String telefonedeContato) {
+        this.telefonedeContato = telefonedeContato;
+    }
 
     public Pessoa(){
 
--- AppListadesejos/app/src/main/java/devandroid/junior/applistadesejos/view/MainActivity.java
+++ AppListadesejos/app/src/main/java/devandroid/junior/applistadesejos/view/MainActivity.java
@@ -14,9 +14,10 @@ import devandroid.junior.applistadesejos.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    Pessoa outrapessoa;
 
-
-
+    String dadosPessoa;
+    String dadosOutraPessoa;
 
 
     @Override
@@ -26,6 +27,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         pessoa = new Pessoa();
+        outrapessoa = new Pessoa();
+
+        //atribuido valores ao Objeto pessoa.
+        //conforme O Template(TEMPLATE ESTA EM Pessoa.java)
+
+        pessoa.setPrimeiroNome("Marco");
+        pessoa.setSobreNome("Ribeiro");
+        pessoa.setNomedoCurso("AndroidApp");
+        pessoa.setTelefonedeContato("1199254985");
+
+        outrapessoa.setPrimeiroNome("Junior");
+        outrapessoa.setSobreNome("Antonio");
+        outrapessoa.setNomedoCurso("Andoid2024");
+        outrapessoa.setTelefonedeContato("11-9989598");
+
+        dadosPessoa = " \nPrimeiro nome:\n\n ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " \n\nSobrenome:\n\n ";
+        dadosPessoa += pessoa.getSobreNome();
+        dadosPessoa +=" \n\nNome do curso:\n\n ";
+        dadosPessoa += pessoa.getNomedoCurso();
+        dadosPessoa +=" \n\nTelefone Contato:\n\n ";
+        dadosPessoa +=pessoa.getTelefonedeContato();
+
+
+        dadosOutraPessoa = " \nPrimeiro nome:\n\n ";
+        dadosOutraPessoa += outrapessoa.getPrimeiroNome();
+        dadosOutraPessoa += " \n\nSobrenome:\n\n ";
+        dadosOutraPessoa += outrapessoa.getSobreNome();
+        dadosOutraPessoa +=" \n\nNome do curso:\n\n ";
+        dadosOutraPessoa += outrapessoa.getNomedoCurso();
+        dadosOutraPessoa +=" \n\nTelefone Contato:\n\n ";
+        dadosOutraPessoa +=outrapessoa.getTelefonedeContato();
+
+        int parada = 0;
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
